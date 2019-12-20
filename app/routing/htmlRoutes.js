2905aefa28978ba1f0d58f03dda7e107deae87bf
Zhaoyu Qiao <qiao.zy7@gmail.com>
2019-12-20 00:29:47 -0500

    initial commit with html files, server.js files.

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,17 @@
+// Dependencies:
+// require path package to be able to route url to html files.
+let path = require("path");
+
+
+// Routing:
+// Export a function by using this js, so other file can use the function to handle routing.  
+// This function takes in app as an argument
+module.exports = function (app) {
+
+    // handles the routing for get requests for different urls (survey / home).
+    // A GET Route to /survey which should display the survey page.
+
+
+
+    // A default, catch-all route that leads to home.html which displays the home page.
+}
---
+++ server.js
@@ -0,0 +1,34 @@
+// require the basic npm packages we've used in class: express and path
+let express = require("express");
+let path = require("path");
+
+// tell node we are setting up a express server
+// app is a middleware to handle functions between request and response.
+let app = express();
+
+// set an initial port,we will use this port later in our listener
+let PORT = process.env.PORT || 8080
+
+// Use below two lines set up express app to handle data parsing, for POST and PUT requests.
+// you can also use body-parser npm package
+
+// express.urlencoded() is a method inbuilt in express to recognize the incoming Request Object as strings or arrays.
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// express.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object. 
+app.use(express.json());
+
+// points our server to a series of route files
+// the route files then provides the server a mapping between urls and specific files.
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Listener
+// let the server listen on events happening on port, 
+// upon events happening on the port, do the call back function
+app.listen(PORT, function () {
+    console.log("App listening on port: ", PORT)
+})
